1b21d6229fff4e92d30db0a1b845116d3cbc3dbd
kumervova <kumervova@gmail.com>
2025-10-24 12:06:33 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,43 @@
-const TARGET_HOST = "https://priblivonis.icu";
+const TARGET_HOST = 'https://priblivonis.icu'
 
-Deno.serve(async (req: Request): Promise<Response> => {
-  const url = new URL(req.url);
-  const targetUrl = new URL(url.pathname + url.search, TARGET_HOST);
+Deno.serve(
+	async (req: Request, connInfo: Deno.ServeHandlerInfo): Promise<Response> => {
+		const url = new URL(req.url)
+		const targetUrl = new URL(url.pathname + url.search, TARGET_HOST)
 
-  const proxyReq = new Request(targetUrl.toString(), {
-    method: req.method,
-    headers: req.headers,
-    body: req.method !== "GET" && req.method !== "HEAD" ? req.body : undefined,
-  });
+		const { hostname: clientIp } = connInfo.remoteAddr as Deno.NetAddr
 
-  try {
-    const response = await fetch(proxyReq);
+		const headers = new Headers(req.headers)
 
-    const headers = new Headers(response.headers);
-    headers.set("x-proxy", "deno-deploy");
+		if (clientIp) {
+			const prevXff = headers.get('x-forwarded-for')
+			headers.set(
+				'x-forwarded-for',
+				prevXff ? `${prevXff}, ${clientIp}` : clientIp
+			)
+			headers.set('ip', clientIp)
+		}
 
-    return new Response(response.body, {
-      status: response.status,
-      statusText: response.statusText,
-      headers,
-    });
-  } catch (err) {
-    return new Response("Proxy error: " + err.message, { status: 502 });
-  }
-});
+		const proxyReq = new Request(targetUrl.toString(), {
+			method: req.method,
+			headers: req.headers,
+			body:
+				req.method !== 'GET' && req.method !== 'HEAD' ? req.body : undefined,
+		})
 
+		try {
+			const response = await fetch(proxyReq)
+
+			const headers = new Headers(response.headers)
+			headers.set('x-proxy', 'deno-deploy')
+
+			return new Response(response.body, {
+				status: response.status,
+				statusText: response.statusText,
+				headers,
+			})
+		} catch (err) {
+			return new Response('Proxy error: ' + err.message, { status: 502 })
+		}
+	}
+)
